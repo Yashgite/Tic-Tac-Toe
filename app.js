6ccb41ff434c9e6b44e01f070e9343ab442ffa36
Yashgite <131615395+Yashgite@users.noreply.github.com>
2024-01-19 04:58:45 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,71 @@
+let boxes = document.querySelectorAll(".box");
+let resetbtn = document.querySelector("#reset-btn");
+let newbtn = document.querySelector(".new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
+let turn = true;
+let winPatterns =[[0,1,2],[0,3,6],[0,4,8],[1,4,7],[2,4,6],[2,5,8],[3,4,5],[6,7,8]];   //2d array
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        if(turn === true){
+            box.innerText = "O";
+            box.style.color = "green";
+            turn = false;
+        }
+        else{
+             box.innerText = "X";
+             box.style.color = "red";
+            turn = true;
+        }
+        box.disabled = true;
+
+        checkWinner();
+    });
+});
+
+const resetGame = () => {
+    turn=true;
+    enableboxes();
+
+}
+
+const disableboxes = () => {
+    for(let box of boxes){
+        box.disabled = true;
+    }
+}
+
+const enableboxes = () => {
+    for(let box of boxes){
+        box.disabled= false;
+        box.innerText = '';
+        msgContainer.classList.add("hide");
+    }
+}
+
+const showWinner = (winner) => {
+    msg.innerText =`Congratulation , winner is ${ winner}`;
+    msgContainer.classList.remove("hide");
+    disableboxes();
+};
+
+const checkWinner = () => {
+    for(let pattern of winPatterns) {
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+        
+        if(pos1Val != '' && pos2Val != '' && pos3Val != ''){
+            if(pos1Val === pos2Val && pos2Val === pos3Val){
+                showWinner(pos1Val);
+            }
+        
+        }
+    }
+};
+
+resetbtn.addEventListener("click",resetGame);
+newbtn.addEventListener("click",resetGame);
+    
